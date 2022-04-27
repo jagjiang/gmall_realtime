5605cc0299727c1d0ba51390e954ff7e15c4ba43
jiangbo <mintlolly@outlook.com>
2022-04-27 09:05:39 +0800

    watermark demo

---
+++ src/main/java/com/mintlolly/review/flink/MessageUtil.java
@@ -0,0 +1,75 @@
+package com.mintlolly.review.flink;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+import java.util.Random;
+
+/**
+ * Created on 2022/4/24
+ *
+ * @author jiangbo
+ * Description:
+ */
+public class MessageUtil {
+    public static void main(String[] args) {
+        String broker = "101.42.251.112:9092";
+        String topic = "windows";
+        Properties props = new Properties();
+        Random random = new Random();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,broker);
+        props.setProperty(ProducerConfig.ACKS_CONFIG,"all");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                String message = getMessage();
+                System.out.println(message);
+                producer.send(new ProducerRecord<>(topic, message));
+                producer.flush();
+                try {
+                    Thread.sleep(random.nextInt(10) * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                String message = getMessage();
+                System.out.println(message);
+                producer.send(new ProducerRecord<>(topic, message));
+                producer.flush();
+                try {
+                    Thread.sleep(random.nextInt(10) * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        //多线程发送数据
+
+    }
+    static String getMessage(){
+        String[] names = {"lan", "black", "kee", "yang"};
+        Random random = new Random();
+        String name = names[random.nextInt(names.length)];
+        int score = random.nextInt(10);
+        long date = System.currentTimeMillis();
+        return name + ","+score+","+date;
+    }
+
+    static void sendSocketMessage(String message) throws IOException {
+        Socket socket = new Socket("101.42.251.112",9999);
+        OutputStream out = socket.getOutputStream();
+        out.write(message.getBytes(StandardCharsets.UTF_8));
+        out.flush();
+    }
+}
--- src/main/java/com/mintlolly/review/flink/WindowAndWaterMarkDemo.java
+++ src/main/java/com/mintlolly/review/flink/WindowAndWaterMarkDemo.java
@@ -1,5 +1,6 @@
 package com.mintlolly.review.flink;
 
+import com.mintlolly.utils.MyKafkaUtil;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.AggregateFunction;
@@ -7,10 +8,19 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 /**
  * Created on 2022/4/18
@@ -21,18 +31,36 @@ import java.time.Duration;
 public class WindowAndWaterMarkDemo {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        //socket source
-        DataStreamSource<String> ds = env.socketTextStream("localhost", 9999);
+        env.setParallelism(1);
+        FlinkKafkaConsumer<String> kafkaSource = MyKafkaUtil.getKafkaSource("windows", "test");
+        DataStreamSource<String> ds = env.addSource(kafkaSource.setStartFromEarliest());
+        //设置watermark
         SingleOutputStreamOperator<String> ssoso = ds.assignTimestampsAndWatermarks(WatermarkStrategy
-                .<String>forBoundedOutOfOrderness(Duration.ofSeconds(60))
+                .<String>forBoundedOutOfOrderness(Duration.ofSeconds(10))
                 .withTimestampAssigner(new MyTimeAssigner()));
+        //keyby 打印窗口信息
         KeyedStream<String, String> ks = ssoso.keyBy(f -> f.split(",")[0]);
-        ks.window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
-                .aggregate(new AddAgeAggregate()).print("watermark");
 
+        ks.map(f-> {
+            String[] split = f.split(",");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:SSS");
+            String date = sdf.format(new Date(Long.parseLong(split[2])));
+            return split[0] + "," +split[1] + "," +date;
+        }).print("keyby");
+
+        OutputTag<String> outputTag = new OutputTag<String>("late"){};
+
+        SingleOutputStreamOperator<String> result = ks.window(TumblingEventTimeWindows.of(Time.seconds(20)))
+                .allowedLateness(Time.minutes(1))
+                .sideOutputLateData(outputTag)
+                .aggregate(new AddAgeAggregate(), new MyWindowFunction());
+
+        result.print("window");
+        result.getSideOutput(outputTag).print("late");
+        //滚动窗口
 //        ks.window(TumblingProcessingTimeWindows.of(Time.seconds(10))).aggregate(new AddAgeAggregate()).print("Tumbling");
+//      //滑动窗口
 //        ks.window(SlidingProcessingTimeWindows.of(Time.seconds(20), Time.seconds(5))).aggregate(new AddAgeAggregate()).print("Sliding");
-
         env.execute();
 
         //设置watermark
@@ -55,9 +83,10 @@ public class WindowAndWaterMarkDemo {
         @Override
         public String add(String value, String acc) {
             String[] split = value.split(",");
-
             int sum = Integer.parseInt(acc.split(",")[1]) + Integer.parseInt(split[1]);
-            return split[0] + "," + sum + ","+split[2];
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String date = sdf.format(new Date(Long.parseLong(split[2])));
+            return split[0] + "," + sum+ ","+date;
         }
 
         @Override
@@ -68,7 +97,20 @@ public class WindowAndWaterMarkDemo {
         @Override
         public String merge(String a, String b) {
             int sum = Integer.parseInt(a.split(",")[1]) + Integer.parseInt(b.split(",")[1]);
-            return a.split(",")[0] + "," + sum+ ","+a.split(",")[2];
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:SSS");
+            String date = sdf.format(new Date(Long.parseLong(a.split(",")[2])));
+            return a.split(",")[0] + "," + sum+ ","+date;
+        }
+    }
+
+    private static class MyWindowFunction implements WindowFunction<String,String,String, TimeWindow> {
+
+        @Override
+        public void apply(String s, TimeWindow window, Iterable<String> input, Collector<String> out) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String start = sdf.format(new Date(window.getStart()));
+            String end = sdf.format(new Date(window.getEnd()));
+            out.collect(start+"-"+end+","+input.iterator().next());
         }
     }
 }
